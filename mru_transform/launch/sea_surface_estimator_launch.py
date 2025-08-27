71d6a5e42f1dec119ef2b25982370c79e79068e5
Roland Arsenault <roland@ccom.unh.edu>
2025-08-27 14:20:40 -0400

    Enable color output in launch gui

--- mru_transform/launch/sea_surface_estimator_launch.py
+++ mru_transform/launch/sea_surface_estimator_launch.py
@@ -16,6 +16,7 @@ def generate_launch_description():
             namespace='',
             respawn=True,
             respawn_delay=2,
+            emulate_tty=True
         ),
         LifecycleTransition(
             lifecycle_node_names=(
